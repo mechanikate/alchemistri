529d157ab943528cefc80a55f16b19713ec36944
mechanikate <cjmatthy200@gmail.com>
2024-02-06 19:11:05 -0500

    add configs!

---
+++ Config/AlchemistriConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+
+namespace alchemistri.Config
+{
+    class AlchemistriConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+        public static AlchemistriConfig Instance => ModContent.GetInstance<AlchemistriConfig>();
+
+        #region recipes
+
+        [DefaultValue(true)]
+        [ReloadRequired]
+        public bool EnableAncientManipulatorRecipe;
+
+        [DefaultValue(true)]
+        [ReloadRequired]
+        public bool EnableLuminiteRecipe;
+
+        #endregion recipes
+    }
+}
--- Content/AlchemistriRecipes.cs
+++ Content/AlchemistriRecipes.cs
@@ -1,5 +1,6 @@
 ﻿using alchemistri.Content.Items;
 using alchemistri.Content.Tiles;
+using static alchemistri.Config.AlchemistriConfig;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -11,17 +12,22 @@ namespace alchemistri.Content
 
         public override void AddRecipes()
         {
-            Recipe ancientManipulatorRecipe = Recipe.Create(ItemID.LunarCraftingStation, 2)
-                                                    .AddIngredient(ItemID.LunarBar, 10)
-                                                    .AddIngredient(ItemID.SpectreBar, 20)
-                                                    .AddTile(TileID.LunarCraftingStation)
-                                                    .Register();
-            Recipe luminiteBarRecipe = Recipe.Create(ItemID.LunarBar, 65)
+            if (Instance.EnableAncientManipulatorRecipe)
+            {
+                Recipe ancientManipulatorRecipe = Recipe.Create(ItemID.LunarCraftingStation, 2)
+                                                        .AddIngredient(ItemID.LunarBar, 10)
+                                                        .AddIngredient(ItemID.SpectreBar, 20)
+                                                        .AddTile(TileID.LunarCraftingStation)
+                                                        .Register();
+            }
+            if (Instance.EnableLuminiteRecipe)
+            {
+                Recipe luminiteBarRecipe = Recipe.Create(ItemID.LunarBar, 65)
                                              .AddIngredient(ItemID.LunarBar, 50)
                                              .AddIngredient(ModContent.ItemType<AntiHydrogen>(), 1)
                                              .AddTile(ModContent.TileType<AntimatterSynthesizerTile>())
                                              .Register();
-
+            }
         }
     }
 }
